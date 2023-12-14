102f1054740c053d6a4fc9320cfbe35ca000e5b1
Constantine Smirnov <constantines748@gmail.com>
2023-12-14 09:54:43 +0300

    Осталось сделать корректные ответы

--- Enigma/4CourseProjectEnigma/EnigmaProject/View/QuizeAfterLessons.xaml.cs
+++ Enigma/4CourseProjectEnigma/EnigmaProject/View/QuizeAfterLessons.xaml.cs
@@ -3,14 +3,16 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Collections.Generic;
 
 namespace EnigmaProject.View
 {
     public partial class QuizeAfterLessons : Page
     {
-        string selectedAnswer = "\0";
-        private ObservableCollection<string> QuestionForAnswers { get; set; }
+        private ObservableCollection<string> QuestionForAnswers;
+        private List<string> CorrectAnswers; // Список для хранения правильных ответов
         private int currentQuestionIndex = 0;
+        private string selectedAnswer = "";
 
         public QuizeAfterLessons()
         {
@@ -22,46 +24,70 @@ namespace EnigmaProject.View
                 "Ответ3"
             };
 
+            // Инициализация списка правильных ответов
+            CorrectAnswers = new List<string>()
+            {
+                "Правильный ответ на вопрос 1",
+                "Правильный ответ на вопрос 2",
+                "Правильный ответ на вопрос 3"
+            };
+
             DataContext = QuestionForAnswers;
             control.ItemsSource = EnigmaBase.GetContext().Questions.Select(q => q.Content).ToList();
             ShowCurrentQuestion();
         }
-        
+
         private string GetCurrentQuestion()
         {
             if (currentQuestionIndex >= 0 && currentQuestionIndex < QuestionForAnswers.Count)
                 return QuestionForAnswers[currentQuestionIndex];
-            return null; 
+            return null;
         }
+
         private void ShowCurrentQuestion()
         {
             if (QuestionForAnswers.Count > 0 && currentQuestionIndex < QuestionForAnswers.Count)
                 QuestionTextBlock.Text = QuestionForAnswers[currentQuestionIndex];
         }
+
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
             var radioButton = sender as RadioButton;
             if (radioButton != null)
                 selectedAnswer = radioButton.Content.ToString();
         }
+
         private void CheckAnswer()
         {
-            // Получить текущий вопрос
-            var currentQuestion = GetCurrentQuestion(); 
-            if (GetCurrentQuestion() != null && GetCurrentQuestion() == selectedAnswer)
+            var currentQuestion = GetCurrentQuestion();
+            if (currentQuestion != null)
             {
-                MessageBox.Show("Правильный ответ!");
+                var correctAnswer = CorrectAnswers[currentQuestionIndex];
+                if (selectedAnswer == correctAnswer)
+                {
+                    MessageBox.Show("Правильный ответ!");
+                }
+                else
+                {
+                    MessageBox.Show("Неправильный ответ. Попробуйте еще раз.");
+                }
+
                 currentQuestionIndex++;
                 if (currentQuestionIndex >= QuestionForAnswers.Count)
                     currentQuestionIndex = 0; // Если дошли до конца списка вопросов, начинаем сначала
+
                 ShowCurrentQuestion();
             }
             else
-                MessageBox.Show("Неправильный ответ. Попробуйте еще раз.");    
+            {
+                MessageBox.Show("Некорректный вопрос или ответ не найден.");
+            }
         }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             CheckAnswer();
         }
     }
 }
+
